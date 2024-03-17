6394731a16fa454df9879d661e50d187c4ac055b
Edyta Boczarska <edyta.boczarska@gate-software.com>
2024-03-18 00:39:03 +0100

    Status details - connection to the database

--- src/components/StudentPanel/Status/Status.js
+++ src/components/StudentPanel/Status/Status.js
@@ -1,14 +1,35 @@
 // Status.js
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Status.module.css';
 import TopMenu from "../TopMenu/TopMenu";
+import axios from "axios";
+import {useAuth} from "../../../hooks/useAuth";
+import {useNavigate} from "react-router-dom";
 
-const scholarshipData = [
+/*const scholarshipData = [
     { date: '2023-01-01', type: 'socjalne', status: 'Odrzucone', id: 1 },
     { date: '2023-02-15', type: 'rektorskie', status: 'Zaakceptowane', id: 2 },
-];
+];*/
 
 const Status = () => {
+    const [scholarshipData, setScholarshipData]=useState([]);
+    const navigate = useNavigate(); // Używamy useNavigate tutaj
+
+    useAuth();
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/applications') // Zastąp 'localhost:3000' rzeczywistym adresem Twojego serwera backendowego
+            .then(response => {
+                setScholarshipData(response.data);
+            })
+            .catch(error => {
+                console.error("There was an error fetching the scholarship data:", error);
+            });
+    }, []);
+    const handleViewDetails = (id) => {
+        navigate(`/details/${id}`); // Użyj ścieżki zgodnej z konfiguracją Twojego routingu
+    };
+
     return (
 
         <div className="application-container">
@@ -27,12 +48,11 @@ const Status = () => {
                     <tbody>
                     {scholarshipData.map((item) => (
                         <tr key={item.id}>
-                            <td className={styles.td}>{item.date}</td>
-                            <td className={styles.td}>{item.type}</td>
+                            <td className={styles.td}>{new Date(item.createdAt).toLocaleDateString()}</td>                             <td className={styles.td}>{item.type}</td>
                             <td className={styles.td}>{item.status}</td>
                             <td className={styles.td}>
                                 <button className={styles.button}
-                                        onClick={() => alert(`Wybrano stypendium ${item.id}`)}>Zobacz
+                                        onClick={() => handleViewDetails(item.id)}>Zobacz
                                 </button>
                             </td>
                         </tr>
